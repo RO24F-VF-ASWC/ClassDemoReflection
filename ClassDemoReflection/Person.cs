eef81ab0a54aa10bd2b8940bf43c50cc3b413e9b
Peter Levinsky <pele@zealand.dk>
2024-02-27 10:20:20 +0100

    Add project files.

---
+++ ClassDemoReflection/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoReflection
+{
+    public class Person:IPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Person(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Person():this(-1,"Dummy")
+        {
+            //Id = -1;
+            //Name = "Dummy";
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Name)}={Name}}}";
+        }
+    }
+}
---
+++ ClassDemoReflection/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ClassDemoReflection;
+using System.Reflection;
+
+Person person = new Person(2,"Peter");
+Console.WriteLine(person);
+
+
+
+TryReflection(person);
+
+void TryReflection(object obj)
+{
+    Type t = obj.GetType();
+
+    Console.WriteLine("Name:" + t.FullName);
+    Console.WriteLine("Interface:" + t.IsInterface);
+    Console.WriteLine("Clas:" + t.IsClass);
+    Console.WriteLine("Abstract:" + t.IsAbstract);
+    Console.WriteLine("Base:" + t.BaseType);
+
+
+    Console.WriteLine(" ==> Properties <=====");
+    foreach (PropertyInfo pi in t.GetProperties())
+    {
+        Console.WriteLine(pi);
+    }
+
+    Console.WriteLine(" ===> Metoder <====");
+    foreach(MethodInfo mi in t.GetMethods())
+    {
+        Console.WriteLine(mi);
+    }
+
+
+    Console.WriteLine(" ===> Call method <=======");
+    var setId = t.GetMethods().First(m => m.Name == "set_Id");
+
+    Console.WriteLine(" BEFORE " + obj);
+
+    setId.Invoke(obj, new object?[] { 149 });
+
+    Console.WriteLine(" After " + obj);
+
+
+
+
+}
